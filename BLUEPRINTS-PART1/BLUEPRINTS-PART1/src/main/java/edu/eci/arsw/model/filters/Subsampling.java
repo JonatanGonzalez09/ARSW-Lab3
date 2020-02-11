41dd2d477fc986268715291796f62b8f2a6d50b4
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-02-11 15:50:24 -0500

    Filtro Subsampling

--- BLUEPRINTS-PART1/BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/model/filters/Subsampling.java
+++ BLUEPRINTS-PART1/BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/model/filters/Subsampling.java
@@ -5,18 +5,27 @@
  */
 package edu.eci.arsw.model.filters;
 
+import java.util.HashSet;
 import java.util.Set;
 import edu.eci.arsw.blueprints.model.*;
 
 
-/**
- *
- * @author david.caycedo
- */
 public class Subsampling implements filter{
+
+    Set<Blueprint> lista = new HashSet<>();
 
     @Override
     public Set<Blueprint> filtrate(Set<Blueprint> blueprints) {
+        int i=0;
+        for (Blueprint bp : blueprints) {
+            for (Point p : bp.getPoints()){
+                if (i%2==0){
+                    bp.getPoints().remove(p);
+                    i++;
+                }
+                i++;
+            }
+        }
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
